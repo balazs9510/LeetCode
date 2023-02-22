eb97cbfacf38179ed5dc8c3aa46a5e745097fb7d
BALAZS BERTA <95berta.balazs@gmail.com>
2023-02-22 19:33:30 +0100

    Roman to integer

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/RomanToInteger/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges.Easy.RomanToInteger
+{
+    public class Solution
+    {
+        Dictionary<char, int> Map = new Dictionary<char, int>
+        {
+            { 'I', 1},
+            { 'V', 5},
+            { 'X', 10},
+            { 'L', 50},
+            { 'C', 100},
+            { 'D', 500},
+            { 'M', 1000},
+        };
+
+        public int RomanToInt(string s)
+        {
+            var result = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                var current = Map[s[i]];
+
+                if (current < Map[s[i + 1]])
+                {
+                    result -= current;
+                }
+                else
+                {
+                    result += current;
+                }
+
+            }
+            result += Map[s[s.Length - 1]];
+            return result;
+        }
+    }
+}
---
+++ LeetCodeChallenges/LeetCodeChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeChallenges.RomanToInteger;
+
+
+var solution = new Solution();
+
+Console.WriteLine(solution.RomanToInt("I"));
+
+Console.WriteLine(solution.RomanToInt("I"));
+Console.WriteLine(solution.RomanToInt("III"));
+Console.WriteLine(solution.RomanToInt("LVIII"));
+Console.WriteLine(solution.RomanToInt("MCMXCIV"));
