412d8de2e7a807be7461294729ef46c0fed7f306
BALAZS BERTA <95berta.balazs@gmail.com>
2023-02-24 18:53:22 +0100

    MaxProductDifference implementation

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/MaxProductDifference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges.Easy
+{
+    public class Solution
+    {
+        public int MaxProductDifference(int[] nums)
+        {
+            Array.Sort(nums);
+
+            return nums[nums.Length - 1] * nums[nums.Length - 2] - nums[1] * nums[0];
+        }
+
+    }
+}
