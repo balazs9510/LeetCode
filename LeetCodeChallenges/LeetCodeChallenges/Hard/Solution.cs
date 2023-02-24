13fdb711a7abeeb498c3eab5824103630a65f877
BALAZS BERTA <95berta.balazs@gmail.com>
2023-02-24 15:38:19 +0100

    Non optimal search

---
+++ LeetCodeChallenges/LeetCodeChallenges/Hard/Solution.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges.Hard
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            bool isEven = (nums1.Length + nums2.Length) % 2 == 0;
+            int middle = (nums1.Length + nums2.Length) / 2 + 1;
+            int i1 = 0, i2 = 0;
+            double res = 0;
+
+            for (int i = 0; i < middle; i++)
+            {
+                if (i1 >= nums1.Length)
+                {
+                    if (!isEven && i == middle - 1) return nums2[i2];
+
+                    if (i == middle - 2)
+                    {
+                        res = nums2[i2];
+                    }
+                    if (i == middle - 1)
+                    {
+                        res += nums2[i2];
+                    }
+                    i2++;
+                    continue;
+                }
+                if (i2 >= nums2.Length)
+                {
+                    if (!isEven && i == middle - 1) return nums1[i1];
+
+                    if (i == middle - 2)
+                    {
+                        res = nums1[i1];
+                    }
+                    if (i == middle - 1)
+                    {
+                        res += nums1[i1];
+                    }
+                    i1++;
+                    continue;
+                }
+
+
+                int num1 = nums1[i1];
+                int num2 = nums2[i2];
+                if (num1 < num2)
+                {
+                    if (!isEven && i == middle - 1) return nums1[i1];
+
+                    if (i == middle - 2)
+                    {
+                        res = nums1[i1];
+                    }
+                    if (i == middle - 1)
+                    {
+                        res += nums1[i1];
+                    }
+                    i1++;
+                }
+                else
+                {
+                    if (!isEven && i == middle - 1) return nums2[i2];
+
+                    if (i == middle - 2)
+                    {
+                        res = nums2[i2];
+                    }
+                    if (i == middle - 1)
+                    {
+                        res += nums2[i2];
+                    }
+                    i2++;
+                }
+            }
+
+            return res / (double)2;
+        }
+    }
+}
--- LeetCodeChallenges/LeetCodeChallenges/Program.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeChallenges.Hard;
 
-using LeetCodeChallenges.Medium.LengthOfLongestSubstring;
-
-var solution = new Solution();
-
-Console.WriteLine(solution.LengthOfLongestSubstring("abcabcbb"));
-Console.WriteLine(solution.LengthOfLongestSubstring("bbbbb"));
-Console.WriteLine(solution.LengthOfLongestSubstring("pwwkew"));
-Console.WriteLine(solution.LengthOfLongestSubstring("bbtablud"));
+var sol = new Solution();
+Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 10, 30, 50, 70, 90 }, new int[] { 0, 20, 40, 60, 80, 100, 120 }));
+Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 0, 1 }, new int[] { 2, 3, }));
+Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 0, 1 }, new int[] { 2, 3, 10 }));
+Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 1,3 }, new int[] { 2 }));
+Console.WriteLine(sol.FindMedianSortedArrays(new int[] { 0,1,2,3,4,5 }, new int[] { 8,10,20,21,52,100 }));
