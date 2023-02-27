8e0cca53b503be39d8d617c9a4acdf0a5375b306
BALAZS BERTA <95berta.balazs@gmail.com>
2023-02-27 20:21:39 +0100

    LongestPalindrome implementation

---
+++ LeetCodeChallenges/LeetCodeChallenges/Medium/LongestPalindrome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenges.Medium
+{
+    public partial class MediumSolution
+    {
+        public string LongestPalindrome(string s)
+        {
+            return LongestPalindromeRecursive(s, s.Length);
+        }
+
+        private string LongestPalindromeRecursive(string s, int length)
+        {
+            if (length == 0) { return ""; }
+
+            for (int i = 0; i <= s.Length - length; i++)
+            {
+                string toCheck = s.Substring(i, length);
+                if (IsPalindrome(toCheck)) { return toCheck; }
+            }
+            return LongestPalindromeRecursive(s, length - 1);
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static void LongestPalindromeTest()
+        {
+            var sol = new MediumSolution();
+            Console.WriteLine(sol.LongestPalindrome("babad"));
+            Console.WriteLine(sol.LongestPalindrome("cbbd"));
+        }
+
+    }
+}
--- LeetCodeChallenges/LeetCodeChallenges/Program.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCodeChallenges.Easy;
+using LeetCodeChallenges.Medium;
 
-Solution.LongestCommonPrefixTest();
+MediumSolution.LongestPalindromeTest();
