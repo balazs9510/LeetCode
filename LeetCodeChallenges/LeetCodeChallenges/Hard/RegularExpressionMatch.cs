ffdbe9fae23415a713562f824519b2367c54b8ba
Balazs Berta <balazs.berta@playngo.com>
2023-03-02 15:06:03 +0100

    not everything matchinng

--- LeetCodeChallenges/LeetCodeChallenges/Hard/RegularExpressionMatch.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Hard/RegularExpressionMatch.cs
@@ -8,47 +8,34 @@ namespace LeetCodeChallenges.Hard
 {
 	public partial class HardSolution
 	{
+
 		public bool IsMatchingRegularExpression(string s, string p)
 		{
-			string[] quantifierGroups = p.Split('*');
-			int quantifierGroupsIterator = 0;
-			int groupIterator = 0;
-			for (int i = 0; i < s.Length; i++)
+			return RegexRecursive(s, p, s.Length - 1, p.Length - 1);
+		}
+
+		private bool RegexRecursive(string s, string p, int sIterator, int pIterator)
+		{
+			// both string checked trough
+			if (sIterator < 0 && pIterator < 0) return true;
+			// p fully checked but s not or s fully checked but p not and it doesn't a quantifier end
+			if (pIterator < 0 || (sIterator < 0 && p[pIterator] != '*')) return false;
+			// s fully checked but quantifier at the end so we have to check if there are still more items in p
+			if (sIterator < 0 && p[pIterator] == '*') return RegexRecursive(s, p, sIterator, pIterator - 2);
+
+			if (s[sIterator] == p[pIterator] || p[pIterator] == '.') return RegexRecursive(s, p, sIterator - 1, pIterator - 1);
+			// if the current symbols are not matching and the pattern doesn't end to asterisk
+			if (p[pIterator] != '*') return false;
+
+			if (s[sIterator] == p[pIterator - 1] || p[pIterator - 1] == '.')
 			{
-				char currentChar = s[i];
-				string group = quantifierGroups[quantifierGroupsIterator];
-				if (group.Length == 0 || groupIterator >= group.Length) return false;
-
-				char toCheck = group[groupIterator];
-				if (groupIterator < group.Length - 1 ||
-					quantifierGroupsIterator == quantifierGroups.Length - 1)
-				{
-					if (groupIterator >= group.Length) return false;
-					if (toCheck != '.' && toCheck != currentChar) return false;
-
-					groupIterator++;
-				}
-				else
-				{
-					if (toCheck != '.' && toCheck != currentChar)
-					{
-						quantifierGroupsIterator++;
-						groupIterator = 0;
-						i--;
-					}
-
-					if (quantifierGroupsIterator + 1 < quantifierGroups.Length)
-					{
-						string nextGroup = quantifierGroups[quantifierGroupsIterator + 1];
-						if (nextGroup.Length > 0 && nextGroup[0] == toCheck)
-						{
-							quantifierGroups[quantifierGroupsIterator + 1] = nextGroup.Substring(1);
-						}
-					}
-				}
+				var result = RegexRecursive(s, p, sIterator - 1, pIterator);
+				if (!result) return RegexRecursive(s, p, sIterator, pIterator - 2);
+				
+				return true;
 			}
 
-			return quantifierGroups.Last().Length == groupIterator;
+			return false;
 		}
 
 		public static void IsMatchingRegularExpressionTests()
@@ -56,18 +43,21 @@ namespace LeetCodeChallenges.Hard
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("c", "a*b*c"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaabc", "a*b*c"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaabc", "a*ab*c"));
-			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaa", "a*a"));
-			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aa", "a*"));
 			//WriteTestToConsole(false, Instance.IsMatchingRegularExpression("aa", "a*b"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aab", "a*b"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaaaaaaaab", "a*b"));
-			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("b", "a*b"));
-			//WriteTestToConsole(false, Instance.IsMatchingRegularExpression("cb", "a*b"));
-			//WriteTestToConsole(false, Instance.IsMatchingRegularExpression("bb", "a*b"));
-			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("ab", "a.*b"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("adsadsadadab", "a.*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("mississippi", "mis*is*ip*."));
 			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("adsadsadadabc", "a.*bc"));
 			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("adsadsadadabc", "a.*b"));
+			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("cb", "a*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("ab", "a.*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaa", "a*a"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("b", "a*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aab", "a*b"));
+			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("aa", "a*b"));
+			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("bb", "a*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aa", "a*"));
 			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("aa", "a"));
 			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaba", "aa.a"));
 			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aa", "a."));
