cbde228d61fcea46f4eadc9c1c8d1c7dffb65153
Balazs Berta <balazs.berta@playngo.com>
2023-03-02 18:52:58 +0100

    Initial DP solution still not mathcing

--- LeetCodeChallenges/LeetCodeChallenges/Hard/RegularExpressionMatch.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Hard/RegularExpressionMatch.cs
@@ -11,32 +11,55 @@ namespace LeetCodeChallenges.Hard
 
 		public bool IsMatchingRegularExpression(string s, string p)
 		{
-			return RegexRecursive(s, p, s.Length - 1, p.Length - 1);
-		}
+			if (s.Length == 0 && p.Length != 0) return false;
 
-		private bool RegexRecursive(string s, string p, int sIterator, int pIterator)
-		{
-			// both string checked trough
-			if (sIterator < 0 && pIterator < 0) return true;
-			// p fully checked but s not or s fully checked but p not and it doesn't a quantifier end
-			if (pIterator < 0 || (sIterator < 0 && p[pIterator] != '*')) return false;
-			// s fully checked but quantifier at the end so we have to check if there are still more items in p
-			if (sIterator < 0 && p[pIterator] == '*') return RegexRecursive(s, p, sIterator, pIterator - 2);
-
-			if (s[sIterator] == p[pIterator] || p[pIterator] == '.') return RegexRecursive(s, p, sIterator - 1, pIterator - 1);
-			// if the current symbols are not matching and the pattern doesn't end to asterisk
-			if (p[pIterator] != '*') return false;
-
-			if (s[sIterator] == p[pIterator - 1] || p[pIterator - 1] == '.')
+			var dp = new bool[s.Length + 1][];
+			for (int i = 0; i <= s.Length; i++)
 			{
-				var result = RegexRecursive(s, p, sIterator - 1, pIterator);
-				if (!result) return RegexRecursive(s, p, sIterator, pIterator - 2);
-				
-				return true;
+				dp[i] = new bool[p.Length + 1];
 			}
 
-			return false;
+			dp[0][0] = true;
+			for (int i = 1; i <= p.Length; i++)
+			{
+				if (p[i - 1] == '*') dp[0][i] = dp[0][i - 2];
+			}
+
+			for (int i = 1; i <= s.Length; i++)
+			{
+				for (int j = 1; j <= p.Length; j++)
+				{
+					if (s[i - 1] == p[j - 1] || p[j - 1] == '.')
+					{
+						dp[i][j] = dp[i - 1][j - 1];
+					}
+					else if (p[j - 1] == '*')
+					{
+						if (p[j - 2] == s[i - 1] || p[j - 2] == '.')
+						{
+							dp[i][j] = dp[i - 1][j];
+						}
+
+
+
+						dp[i][j] |= dp[i][j - 2];
+
+					}
+					else
+					{
+						dp[i][j] = false;
+					}
+				}
+			}
+
+			//for (int i = 0; i < dp.Length; i++)
+			//{
+			//	Console.WriteLine(string.Join(",", dp[i].Select(x => x ? 1 : 0)));
+			//}
+			return dp[s.Length - 1][p.Length];
 		}
+
+
 
 		public static void IsMatchingRegularExpressionTests()
 		{
@@ -47,6 +70,7 @@ namespace LeetCodeChallenges.Hard
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aab", "a*b"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("aaaaaaaaab", "a*b"));
 			//WriteTestToConsole(true, Instance.IsMatchingRegularExpression("adsadsadadab", "a.*b"));
+			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("ab", "ab*"));
 			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("mississippi", "mis*is*ip*."));
 			WriteTestToConsole(true, Instance.IsMatchingRegularExpression("adsadsadadabc", "a.*bc"));
 			WriteTestToConsole(false, Instance.IsMatchingRegularExpression("adsadsadadabc", "a.*b"));
