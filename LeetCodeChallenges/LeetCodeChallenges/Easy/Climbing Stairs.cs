97515f1b34027d5ef364fc12dc3a0ff63b52cbd0
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-22 13:18:42 +0200

    climbing stairs memo

--- LeetCodeChallenges/LeetCodeChallenges/Easy/Climbing Stairs.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Climbing Stairs.cs
@@ -10,33 +10,41 @@ namespace LeetCodeChallenges.Easy
 {
     public class Climbing_Stairs
     {
-        private readonly ITestOutputHelper output;
-
-        public Climbing_Stairs(ITestOutputHelper output)
-        {
-            this.output = output;
-        }
 
         public int ClimbStairs(int n)
         {
-            return ClimbStairsRecursive(n, 0, 0);
+            var memo = new int?[45];
+            return ClimbStairsRecursive(n, 0, memo);
         }
 
-        private int ClimbStairsRecursive(int stairCount, int currentCount, int distinctWayCount)
+
+        // TODO use memo and stairs left
+        private int ClimbStairsRecursive(int stairsLeft, int distinctWayCount, int?[] memo)
         {
             int[] steps = new int[] { 1, 2 };
             foreach (int step in steps)
             {
-                if (currentCount + step < stairCount)
-                {
-                    distinctWayCount = ClimbStairsRecursive(stairCount, currentCount + step, distinctWayCount);
-                }
-                else if (currentCount + step == stairCount)
-                {
-                    distinctWayCount++;
-                }
-            }
+                if (step > stairsLeft) continue;
 
+                if (memo[stairsLeft - step] != null)
+                {
+                    distinctWayCount += memo[stairsLeft - step].Value;
+                }
+                else
+                {
+                    if (step < stairsLeft)
+                    {
+                        distinctWayCount = ClimbStairsRecursive(stairsLeft - step, distinctWayCount, memo);
+                    }
+                    else if (step == stairsLeft)
+                    {
+                        distinctWayCount++;
+                    }
+                }
+
+            }
+            if (memo[stairsLeft] == null)
+                memo[stairsLeft] = distinctWayCount;
             return distinctWayCount;
         }
 
@@ -44,7 +52,7 @@ namespace LeetCodeChallenges.Easy
         [InlineData(2, 2)]
         [InlineData(3, 3)]
         [InlineData(5, 8)]
-        [InlineData(44, 1000000)]
+        [InlineData(44, 1134903170)]
         public void ClimbStairsTests(int input, int expectedOutcome)
         {
             // Arrange & act
