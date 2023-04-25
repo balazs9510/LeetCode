2613b835dbf9fd70f6fadf1a1f29be778e0d0705
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-25 19:21:27 +0200

    3Sums, and Remove Nth node from end

--- LeetCodeChallenges/LeetCodeChallenges/Medium/3Sum.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Medium/3Sum.cs
@@ -3,26 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace LeetCodeChallenges.Medium
 {
     public class _3Sum
     {
-        //public IList<IList<int>> ThreeSum(int[] nums)
-        //{
-        //    var res = new List<IList<int>>();
-        //    var lookUp = new Dictionary<int, List<int>>();
-        //    for (int i = 0; i < nums.Length - 2; i++)
-        //    {
-        //        var first = nums[i];
-        //        for (int j = i + 1; j < nums.Length; j++)
-        //        {
-        //            var current = nums[j];
-        //            var sum = first + current;
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var res = new List<IList<int>>();
+            var resDistinct = new Dictionary<string, bool>();
+            var lookUp = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                var first = nums[i];
+                lookUp = new Dictionary<int, int>();
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var current = nums[j];
+                    var sum = first + current;
+                    var search = 0 - sum;
 
+                    if (lookUp.ContainsKey(search))
+                    {
+                        var items = new List<int> { first, search, current };
+                        items.Sort();
+                        var key = string.Join(' ', items); ;
+                        if (!resDistinct.ContainsKey(key))
+                        {
+                            resDistinct.Add(key, true);
+                            res.Add(items);
+                        }
+                    }
 
-        //        }
-        //    }
-        //}
+                    if (lookUp.ContainsKey(current))
+                    {
+                        lookUp[current]++;
+                    }
+                    else
+                    {
+                        lookUp.Add(current, 1);
+                    }
+
+                }
+            }
+
+            return res;
+        }
+
+        [Theory]
+        [InlineData(new int[] { -1, 0, 1, 2, -1, -4 })]
+        [InlineData(new int[] { 0, 1, 1 })]
+        [InlineData(new int[] { 0, 0, 0 })]
+        public void ThreeSumTests(int[] nums)
+        {
+            // Arrange & act
+            var res = ThreeSum(nums);
+
+            // assert
+        }
     }
 }
---
+++ LeetCodeChallenges/LeetCodeChallenges/Medium/Remove Nth Node From End of List0.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static LeetCodeChallenges.Easy.Remove_Duplicates_from_Sorted_List;
+
+namespace LeetCodeChallenges.Medium
+{
+    public class Remove_Nth_Node_From_End_of_List0
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var index = 0;
+            var iterator = head;
+            var dict = new Dictionary<int, ListNode>();
+            while(iterator != null )
+            {
+                dict.Add(index++, iterator);
+                iterator = iterator.next;
+            }
+            var prev = dict.Count - n - 1;
+            if (prev < 0)
+            {
+                return head.next;
+            }
+            else
+            {
+                dict[prev].next = dict[prev + 1].next;
+            }
+
+            return head;
+        }
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void RemoveNthFromEndTests(ListNode head, int n, ListNode expected)
+        {
+            // Arrance & Act
+            var res = RemoveNthFromEnd(head, n);
+
+            // Assert
+            while (expected != null)
+            {
+                Assert.Equal(expected.val, res.val);
+                expected = expected.next;
+                res = res.next;
+            }
+
+            Assert.Null(res);
+            Assert.Null(expected);
+        }
+
+        public static IEnumerable<object[]> Data()
+        {
+            return new List<object[]>
+           {
+                new object[] { TestData1(), 2, TestDataExpected1()},
+                new object[] { TestData2(), 1, TestDataExpected2()},
+                new object[] { TestData3(), 1, TestDataExpected3()},
+           };
+        }
+
+        public static ListNode TestData1()
+        {
+            var head = new ListNode { val = 1 };
+            head.next = new ListNode { val = 2 };
+            head.next.next = new ListNode { val = 3 };
+            head.next.next.next = new ListNode { val = 4 };
+            head.next.next.next.next = new ListNode { val = 5 };
+            return head;
+        }
+        public static ListNode TestDataExpected1()
+        {
+            var head = new ListNode { val = 1 };
+            head.next = new ListNode { val = 2 };
+            head.next.next = new ListNode { val = 3 };
+            head.next.next.next = new ListNode { val = 5 };
+            return head;
+        }
+        public static ListNode TestData2()
+        {
+            var head = new ListNode { val = 1 };
+           
+            return head;
+        }
+        public static ListNode TestDataExpected2()
+        {
+            return null;
+        }
+        public static ListNode TestData3()
+        {
+            var head = new ListNode { val = 1 };
+            head.next = new ListNode { val = 2 };
+           
+            return head;
+        }
+        public static ListNode TestDataExpected3()
+        {
+            var head = new ListNode { val = 1 };
+           
+            return head;
+        }
+
+    }
+}
