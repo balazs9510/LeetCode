611d25357dc88564b2512aa5f49eda9598021d98
BALAZS BERTA <95berta.balazs@gmail.com>
2023-04-27 19:25:04 +0200

    Bulb switch

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Add Digits.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeChallenges.Easy
+{
+    public class Add_Digits
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10) return num;
+
+            return AddDigits(num.ToString().Select(x => x - '0').Sum());
+        }
+
+        [Theory]
+        [InlineData(38, 2)]
+        [InlineData(0, 0)]
+        [InlineData(32, 5)]
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(999, 9)]
+        public void AddDigitsTests(int num, int expected)
+        {
+            var res = AddDigits(num);
+
+            Assert.Equal(expected, res);
+        }
+    }
+}
---
+++ LeetCodeChallenges/LeetCodeChallenges/Medium/Bulb Switcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeChallenges.Medium
+{
+    public class Bulb_Switcher
+    {
+        public int BulbSwitch(int n)
+        {
+            return (int)Math.Sqrt(n);
+        }
+
+
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(3, 1)]
+        [InlineData(1, 1)]
+        [InlineData(6, 2)]
+        [InlineData(1000000000, 31622)]
+        public void BulbSwitchTests(int n, int expected)
+        {
+            // Arrange & act
+            var res = BulbSwitch(n);
+
+            // Assert
+            Assert.Equal(expected, res);
+        }
+
+    }
+}
