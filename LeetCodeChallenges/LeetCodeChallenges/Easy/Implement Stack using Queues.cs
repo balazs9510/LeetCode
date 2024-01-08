ff78accf376dbf87515125e9f5ee070389bbb6f8
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-08 20:07:16 +0100

    [Stack] Palindrome Linked List

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Implement Stack using Queues.cs
@@ -0,0 +1,107 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Implement_Stack_using_Queues
+{
+    public class MyStack
+    {
+        private Queue<int> _queue = new Queue<int>();
+
+        public MyStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            _queue.Enqueue(x);
+            var count = _queue.Count;
+            for (int i = 0; i < count - 1; i++)
+            {
+                _queue.Enqueue(_queue.Dequeue());
+            }
+        }
+
+        public int Pop()
+        {
+            return _queue.Dequeue();
+        }
+
+        public int Top()
+        {
+            return _queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            return (_queue.Count == 0);
+        }
+    }
+
+    public class MyStackTests
+    {
+        [Fact]
+        public void SimplePushOrderTest()
+        {
+            // Arrange
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            // Act
+            var item = stack.Pop();
+
+            // Assert
+            Assert.Equal(3, item);
+        }
+
+        [Fact]
+        public void DoublePopOrderTest()
+        {
+            // Arrange
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            // Act
+            stack.Pop();
+            var item = stack.Pop();
+
+            // Assert
+            Assert.Equal(2, item);
+        }
+
+
+        [Fact]
+        public void NotEmptyTest()
+        {
+            // Arrange
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(1);
+            stack.Push(1);
+            // Act
+            var res = stack.Empty();
+
+            // Assert
+            Assert.False(res);
+        }
+
+        [Fact]
+        public void EmptyTest()
+        {
+            // Arrange
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Pop();
+            // Act
+            var res = stack.Empty();
+
+            // Assert
+            Assert.True(res);
+        }
+    }
+}
---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Palindrome Linked List.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Palindrome_Linked_List
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // O(n) time, O(n) space
+    public bool IsPalindrome(ListNode head)
+    {
+        var iterator = head;
+        var stack = new Stack<int>();
+        while (iterator != null)
+        {
+            stack.Push(iterator.val);
+            iterator = iterator.next;
+        }
+        iterator = head;
+        while (iterator != null)
+        {
+            if (iterator.val != stack.Pop()) return false;
+            iterator = iterator.next;
+        }
+        return true;
+    }
+
+
+    [Fact]
+    public void IsPalindromeTest1()
+    {
+        // Arrange
+        var root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(2);
+        root.next.next.next = new ListNode(1);
+
+        // Act
+        var res = IsPalindrome(root);
+
+        // Assert
+        Assert.True(res);
+
+    }
+
+    [Fact]
+    public void IsPalindromeTest2()
+    {
+        // Arrange
+        var root = new ListNode(1);
+        root.next = new ListNode(2);
+
+        // Act
+        var res = IsPalindrome(root);
+
+        // Assert
+        Assert.False(res);
+
+    }
+}
