85bdea944047dfbcab9b21a3e7dc9d7467ca8752
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-15 19:40:26 +0100

    [String] Verifying_an_Alien_Dictionary

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Roman to Integer.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Roman_to_Integer
+{
+    Dictionary<char, int> Map = new Dictionary<char, int>
+        {
+            { 'I', 1},
+            { 'V', 5},
+            { 'X', 10},
+            { 'L', 50},
+            { 'C', 100},
+            { 'D', 500},
+            { 'M', 1000},
+        };
+
+    public int RomanToInt(string s)
+    {
+        var result = 0;
+
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            var current = Map[s[i]];
+
+            if (current < Map[s[i + 1]])
+            {
+                result -= current;
+            }
+            else
+            {
+                result += current;
+            }
+
+        }
+        result += Map[s[s.Length - 1]];
+        return result;
+    }
+
+
+    [Theory]
+    [InlineData("III", 3)]
+    [InlineData("LVIII", 58)]
+    [InlineData("MCMXCIV", 1994)]
+    public void RomanToIntTests(string s, int expected)
+    {
+        Assert.Equal(expected, RomanToInt(s));
+    }
+}
---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Verifying an Alien Dictionary.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Verifying_an_Alien_Dictionary : IComparer<string>
+{
+    private string _order;
+
+    public int Compare(string? x, string? y)
+    {
+        int i = 0;
+        while (i < x?.Length && i < y?.Length)
+        {
+            if (x[i] != y[i])
+            {
+                return _order.IndexOf(x[i]) < _order.IndexOf(y[i]) ? -1 : 1;
+            }
+            i++;
+        }
+
+        return x?.Length == y?.Length ? 0 : (x?.Length < y?.Length ? -1 : 1);
+    }
+
+    public bool IsAlienSorted(string[] words, string order)
+    {
+        _order = order;
+        
+        for (int i = 0; i < words.Length - 1; i++)
+        {
+            if (Compare(words[i], words[i + 1]) > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    [Theory]
+    [InlineData(new string[] { "hello", "leetcode" }, "hlabcdefgijkmnopqrstuvwxyz", true)]
+    [InlineData(new string[] { "word", "world", "row" }, "worldabcefghijkmnpqstuvxyz", false)]
+    [InlineData(new string[] { "apple", "app" }, "abcdefghijklmnopqrstuvwxyz", false)]
+    public void IsAlienSortedTests(string[] words, string order, bool expected)
+    {
+        Assert.Equal(expected, IsAlienSorted(words, order));
+    }
+}
