9d2217d0a4d3b0f5374b20b71387ebc4c568e57b
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-19 07:12:46 +0100

    Balanced BT + Max product 3

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Balanced Binary Tree.cs
@@ -0,0 +1,72 @@
+﻿using LeetCodeChallenges.Utils;
+using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Balanced_Binary_Tree
+{
+
+    public bool IsBalanced(TreeNode root)
+    {
+        if (root == null) return true;
+
+        if (GetHeight(root) == -1) return false;
+
+        return true;
+    }
+
+    private int GetHeight(TreeNode node)
+    {
+        if (node is null) return 0;
+
+        var leftHeight = GetHeight(node.left);
+        var rightHeight = GetHeight(node.right);
+
+        if (leftHeight == -1 || rightHeight == -1) return -1;
+        if (Math.Abs(leftHeight - rightHeight) > 1) return -1;
+
+        return Math.Max(leftHeight, rightHeight) + 1;
+    }
+
+
+    [Fact]
+    public void IsBalancedTest1()
+    {
+        // Arrange
+        var root = new TreeNode(3,
+            new TreeNode(9),
+            new TreeNode(20,
+                new TreeNode(15),
+                new TreeNode(7)));
+
+        // Act
+        // Assert
+        Assert.True(IsBalanced(root));
+    }
+
+    [Fact]
+    public void IsBalancedTest2()
+    {
+        // Arrange
+        var root = new TreeNode(1,
+            new TreeNode(2,
+                new TreeNode(3,
+                    new TreeNode(4),
+                    new TreeNode(4)),
+                new TreeNode(3)),
+            new TreeNode(2));
+
+        // Act
+        // Assert
+        Assert.False(IsBalanced(root));
+    }
+
+    [Fact]
+    public void IsBalancedTest3()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.True(IsBalanced(null));
+    }
+}
---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Maximum Product of Three Numbers.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Maximum_Product_of_Three_Numbers
+{
+    public int MaximumProduct(int[] nums)
+    {
+        Array.Sort(nums);
+
+        int n = nums.Length - 1;
+        return Math.Max(nums[0] * nums[1] * nums[n], nums[n - 2] * nums[n - 1] * nums[n]);
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, 6)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 24)]
+    [InlineData(new int[] { -1, -2, -3 }, -6)]
+    [InlineData(new int[] { -1, -2, -3, 50 }, 300)]
+    [InlineData(new int[] { -1, -2, -3, 1 }, 6)]
+    public void MaximumProductTests(int[] nums, int expected)
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Equal(expected, MaximumProduct(nums));
+    }
+}
