f2b7d7b903941fae8628ad44056cc3936f415482
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-21 20:25:03 +0100

    Majority Element + Sorted array to BST

--- LeetCodeChallenges/LeetCodeChallenges/Easy/Convert Sorted Array to Binary Search Tree.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Convert Sorted Array to Binary Search Tree.cs
@@ -1,62 +1,44 @@
-﻿using Xunit;
+﻿using LeetCodeChallenges.Utils;
+using Xunit;
 
 namespace LeetCodeChallenges.Easy
 {
     public class Convert_Sorted_Array_to_Binary_Search_Tree
     {
-        public class TreeNode
-        {
-            public int val;
-            public TreeNode left;
-            public TreeNode right;
-            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-            {
-                this.val = val;
-                this.left = left;
-                this.right = right;
-            }
-        }
-
         public TreeNode SortedArrayToBST(int[] nums)
         {
-            var count = nums.Length;
-            var middle = count / 2;
-            TreeNode prev = null;
-            for (var i = 0; i <= middle; i++)
-            {
-                var current = new TreeNode(nums[i]);
-                if (prev != null) current.left = prev;
-
-                prev = current;
-            }
-            var root = prev;
-            prev = null;
-            for (int i = count - 1; i > middle; i--)
-            {
-                var current = new TreeNode(nums[i]);
-                if (prev != null) current.right = prev;
-                prev = current;
-            }
-            root.right = prev;
-            return root;
+            return SortedArrayToBSTRec(nums, 0, nums.Length - 1);
         }
 
+        private TreeNode SortedArrayToBSTRec(int[] nums, int left, int right)
+        {
+            if (left > right) return null;
+
+            var middle = left + (right - left) / 2;
+            var newNode = new TreeNode(nums[middle]);
+            newNode.left = SortedArrayToBSTRec(nums, left, middle - 1);
+            newNode.right = SortedArrayToBSTRec(nums, middle + 1, right);
+
+            return newNode;
+        }
 
         [Fact]
-        public void MyTestMethod()
+        public void SortedArrayToBSTTest1()
         {
             // Arrange
             var nums = new int[] { -10, -3, 0, 5, 9 };
-
+            var expected = new TreeNode(0,
+                    new TreeNode(-3, new TreeNode(-10)),
+                    new TreeNode(9, new TreeNode(5)));
             // Act
             var result = SortedArrayToBST(nums);
 
             // Assert
-            Assert.True(true);
+            AssertUtils.AssertBinaryTreeEqual(expected, result);
         }
 
         [Fact]
-        public void MyTestMethod2()
+        public void SortedArrayToBSTTest2()
         {
             // Arrange
             var nums = new int[] { -10 };
@@ -65,11 +47,11 @@ namespace LeetCodeChallenges.Easy
             var result = SortedArrayToBST(nums);
 
             // Assert
-            Assert.True(true);
+            AssertUtils.AssertBinaryTreeEqual(new TreeNode(-10), result);
         }
 
         [Fact]
-        public void MyTestMethod3()
+        public void SortedArrayToBSTTest3()
         {
             // Arrange
             var nums = new int[] { 1, 3 };
@@ -78,7 +60,7 @@ namespace LeetCodeChallenges.Easy
             var result = SortedArrayToBST(nums);
 
             // Assert
-            Assert.True(true);
+            AssertUtils.AssertBinaryTreeEqual(new TreeNode(3, new TreeNode(1)), result);
         }
     }
 }
---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Majority Element.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Majority_Element
+{
+    public int MajorityElement(int[] nums)
+    {
+        Array.Sort(nums);
+
+        return nums[nums.Length / 2];
+    }
+
+
+    [Theory]
+    [InlineData(new int[] { 3, 2, 3 }, 3)]
+    [InlineData(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2)]
+    [InlineData(new int[] { 5,6,7,8,5,-1,-3,-32,5,5,5 }, 5)]
+    public void MajorityElementTests(int[] nums, int expected)
+    {
+        Assert.Equal(expected, MajorityElement(nums));
+    }
+}
