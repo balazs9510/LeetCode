2976253cd7907f47c0397c7e9f0618ee7baef8cc
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-24 18:25:22 +0100

    Happy number

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Happy Number.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Happy_Number
+{
+    public bool IsHappy(int n)
+    {
+        bool found = false;
+        var set = new HashSet<int>();
+        while (!found)
+        {
+
+            if (n == 1) found = true;
+
+            var nString = n.ToString().ToArray();
+            //var left = 0; var right = nString.Length - 1;
+            n = nString.Select(x => int.Parse(x.ToString()) * int.Parse(x.ToString())).Sum();
+            if (set.Contains(n)) break;
+            set.Add(n);
+        }
+
+        return found;
+    }
+
+
+    [Theory]
+    [InlineData(19, true)]
+    [InlineData(2, false)]
+    [InlineData(int.MaxValue, false)]
+    public void IsHappyTest(int n, bool expected)
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Equal(expected, IsHappy(n));
+    }
+}
