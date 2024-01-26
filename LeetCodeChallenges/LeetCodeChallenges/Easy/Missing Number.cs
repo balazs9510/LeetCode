4edf2448db27d439cd5b50a21531c7f026bb3f7c
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-26 20:07:06 +0100

    Jewels and Stones, Flatten

--- LeetCodeChallenges/LeetCodeChallenges/Easy/Missing Number.cs
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Missing Number.cs
@@ -6,23 +6,6 @@ namespace LeetCodeChallenges.Easy
     {
         public int MissingNumber(int[] nums)
         {
-            //if (nums.Length == 1) return nums[0] == 0 ? 1 : 0;
-
-            //int min = nums[0], max = nums[0], sum = 0;
-
-            //for (int i = 0; i < nums.Length; i++)
-            //{
-            //    var item = nums[i];
-            //    if (item < min) min = item;
-            //    if (item > max) max = item;
-            //    sum += item;
-            //}
-
-            //var upper = max * (max + 1) / 2;
-            //var lower = (min) * (min + 1) / 2 - min;
-
-            //var res = upper - lower - sum;
-            //return res == 0 ? (max == nums.Length ? min - 1 : max + 1) : res;
             var n = nums.Length;
             return n * (n + 1) / 2 - nums.Sum();
         }
---
+++ LeetCodeChallenges/LeetCodeChallenges/Medium/Flatten Binary Tree to Linked List.cs
@@ -0,0 +1,75 @@
+﻿using LeetCodeChallenges.Utils;
+using Xunit;
+
+namespace LeetCodeChallenges.Medium;
+
+public class Flatten_Binary_Tree_to_Linked_List
+{
+    public void Flatten(TreeNode root)
+    {
+        if (root == null) return;
+        var stack = new Stack<TreeNode>();
+        stack.Push(root);
+        var prev = new TreeNode(-1);
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+
+            if (current.right != null)
+                stack.Push(current.right);
+            if (current.left != null)
+                stack.Push(current.left);
+
+            prev.right = current;
+            current.left = null;
+            prev.left = null;
+            prev = prev.right;
+        }
+    }
+
+
+    [Fact]
+    public void FlattenTest1()
+    {
+        // Arrange
+        var root = new TreeNode(1,
+            new TreeNode(2,
+                new TreeNode(3),
+                new TreeNode(4)),
+            new TreeNode(5,
+                null,
+                new TreeNode(6)));
+
+        var expected = new TreeNode(1,
+            null,
+            new TreeNode(2,
+                null,
+                new TreeNode(3,
+                    null,
+                    new TreeNode(4,
+                        null,
+                        new TreeNode(5,
+                            null,
+                            new TreeNode(6)))))
+            );
+        // Act
+        Flatten(root);
+
+        // Assert
+        AssertUtils.AssertBinaryTreeEqual(expected, root);
+    }
+
+    [Fact]
+    public void FlattenTest2()
+    {
+        // Arrange
+        var root = new TreeNode(0);
+
+        var expected = new TreeNode(0);
+        // Act
+        Flatten(root);
+
+        // Assert
+        AssertUtils.AssertBinaryTreeEqual(expected, root);
+    }
+}
