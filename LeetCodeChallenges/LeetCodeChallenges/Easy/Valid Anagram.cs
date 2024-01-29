4c8d4cd575338e853589664adf05b3b33aaeafd7
BALAZS BERTA <95berta.balazs@gmail.com>
2024-01-29 19:02:01 +0100

    Daily LeetCode: ValidAnagram

---
+++ LeetCodeChallenges/LeetCodeChallenges/Easy/Valid Anagram.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace LeetCodeChallenges.Easy;
+
+public class Valid_Anagram
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        var span = new int[26].AsSpan();
+        for (int i = 0; i < s.Length; i++)
+        {
+            span[s[i] - 'a']++;
+            span[t[i] - 'a']--;
+        }
+
+        foreach (var i in span)
+        {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+
+
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    [InlineData("bab", "bba", true)]
+    public void IsAnagramTests(string s, string t, bool expected)
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+        Assert.Equal(expected, IsAnagram(s, t));
+    }
+}
